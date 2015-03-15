854cd4169f76201a5d4fc76fea431e6671211bd2
Chris Barrett <chris@codesaru.com>
2015-03-15 12:42:53 -0700

    First set of templates.

---
+++ templates/static-lib/__init__.py
@@ -0,0 +1,16 @@
+class StaticLibTemplate(Template):
+	def get_target_dir(self):
+		return os.path.join('src', self.get_target_name() + '-lib')
+
+	def do_replacements(self, s):
+		s = s.replace('LIBNAME', self.get_target_name())
+		return s
+
+	def post_jobs(self):
+		# git init and initial commit
+		subprocess.call(['git', 'init', '.'])
+		subprocess.call(['git', 'add', '-A'])
+		subprocess.call(['git', 'commit', '-m', '"Initial commit."'])
+
+global target_template_object
+target_template_object = StaticLibTemplate()
---
+++ templates/static-lib/assets/src/StdAfx.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "LIBNAME.h"
