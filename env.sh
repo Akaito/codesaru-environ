2dd9d03921baf5639ceecc7b2e042b9616e40548
Chris Barrett <chris@codesaru.com>
2016-02-13 16:33:11 -0800

    Comment-tweak env.sh

--- env.sh
+++ env.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# source this file from the CSaruEnv directory to set up your environment
+# source this file from the CSaruEnv directory to set up your environment.
+# That is: ". env.sh" or "source env.sh".
+# If you execute it ("./env.sh"), it won't do you any good.
 
 export CSaruDir=`pwd`
-if [[ $PATH != *"$CSaruDir"* ]]; then
-	export PATH="${PATH}:${CSaruDir}"
+
+if [[ $PATH != *"${CSaruDir}/bin"* ]]; then
+	export PATH="${PATH}:${CSaruDir}/bin"
 fi
 
