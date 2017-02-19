aa888b63baf267640067d7610b68d24bacb264f6
Chris Barrett <chris@codesaru.com>
2017-02-19 15:36:07 -0800

    Cheap fix for 'tags' as first Make target.
    Return to a common Makefile include later.

--- makefiles/common-stuff.mk
+++
@@ -1,7 +0,0 @@
-tags: ./src/tags
-
-./src/tags:
-	@# tag-relative: Make output relative to ctags file, not current directory
-	@#               Because I run make from repo base dir, and vim from src dir.
-	ctags --recurse --tag-relative -f $@
-
